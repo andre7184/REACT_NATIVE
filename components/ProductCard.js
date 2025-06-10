ee084d23b26efe37ec2bfbacf2652bb428e2ea14
André Martins Brandão <andre53793016@aluno.ms.senac.br>
2025-06-09 20:46:40 -0400

    atualizado

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -3,9 +3,12 @@ import React from 'react';
 import { View, Text, Image } from 'react-native';
 
 const ProductCard = ({ product }) => {
+    
+console.log(product.image);
+
     return (
         <View className="bg-white rounded-lg shadow-md p-2 m-2">
-            <Image source={{ uri: product.image }} className="w-32 h-32 "/>
+            <Image source={{ uri: product.image }}  style={{ width: 126, height: 128, resizeMode: 'contain', alignSelf: 'center' }}/>
             <Text className="text-lg font-bold">{product.title}</Text>
             <Text className="text-lg">{product.category}</Text>
             <Text className="text-gray-600">${product.price}</Text>
